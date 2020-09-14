223fd4b05da27c313a42fe66cafa0ceb2f059272
ricky3knowhere <ricky03senju@gmail.com>
2020-09-14 13:10:54 +0700

    initial commit too

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 3000
 
 app.get('/',(req, res) => res.send('ok'))
 
-app.listen(3000, () => console.log('server runnning'))
+app.listen(port, () => console.log('server running'))
